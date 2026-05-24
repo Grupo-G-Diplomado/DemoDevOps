ca66dae9164c879c3fb1cabf7b628f89ae6d25d9
Alexander8879 <ignacio.as653@gmail.com>
2026-05-24 19:32:54 -0400

    feat(category): add category controller with get, get id and create method

--- Program.cs
+++ Program.cs
@@ -20,6 +20,17 @@ builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+    builder =>
+    {
+        builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+    }
+    );
+}
+);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
